7e05f29b285e1010b6ffc5170e0c5002d6eacddf
Paul O'Connor <jamespauloconnor@gmail.com>
2017-10-03 11:57:13 -0400

    Added Timer

--- src/components/auth/game.js
+++ src/components/auth/game.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
+import Timer from '../timer/Timer';
 
 export default class Game extends Component {
     render() {
       return (
+      	<div>
+      	<Timer />
         <div id="game"></div>
+      	</div>
       );
     }
   }
