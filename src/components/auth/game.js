865cf781d8b9250d4f41abc33d986f1951caf5dd
Aaron Moore <x2kicker2x@gmail.com>
2017-10-04 16:34:52 -0400

    Trying to deploy

--- src/components/auth/game.js
+++ src/components/auth/game.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import Timer from '../timer/Timer';
+import Clock from '../timer/Timer';
 
 export default class Game extends Component {
     render() {
