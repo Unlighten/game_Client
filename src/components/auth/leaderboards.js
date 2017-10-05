7e04aab844e26fab8401a88c621a716b186b803b
Aaron Moore <x2kicker2x@gmail.com>
2017-10-05 15:03:15 -0400

    Trying to fix leaderboard.js

--- src/components/auth/leaderboards.js
+++ src/components/auth/leaderboards.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 // import { ListGroupItem, ListGroup, Button  } from 'react-bootstrap';
 import axios from 'axios';
-import ROOT_URL from '../../actions/index';
 
 
 export default class Leaderboards extends Component {
@@ -16,7 +15,7 @@ export default class Leaderboards extends Component {
 
     // get leaderboard data using axios
     getLeaderBoard(){
-        axios.get(`${ROOT_URL}/leaderboard`, {username, level})
+        axios.get('https://cmdr-server.herokuapp.com/api/leaderboard')
         // axios.get('https://localhost:8080/')
          .then((response) => {
             //  console.log(response.data.data)
